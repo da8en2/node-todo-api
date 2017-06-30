97eaf8403b91bb9e48b46178b1517cd34689b050
Daniel Endres <danielendres@gmail.com>
2017-06-29 23:52:03 -0700

    use object destructuring for mongoclient and ObjectID

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,9 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+//use object destructuring instead:
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
 
 // // example for ES6 object destructuring!
 // var user = {name: 'Dan', age: 35};
