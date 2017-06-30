312b602e0da8e0c3ce5376864be9c5a0424ed640
Daniel Endres <danielendres@gmail.com>
2017-06-29 23:48:16 -0700

    ES6 object destructuring example / test

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
 
+// // example for ES6 object destructuring!
+// var user = {name: 'Dan', age: 35};
+// var {name} = user;
+// console.log(name);
+
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
