4f33be4a17f42dd4f03c9e4ab406640c8e5b1a76
Daniel Endres <danielendres@gmail.com>
2017-07-14 14:17:22 -0700

    setup app for heroku

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 // setup bodyParser as middleware
 app.use(bodyParser.json());
@@ -63,8 +64,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
