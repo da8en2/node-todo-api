aa3eb9de9981059fb3c259f7ce3ac57cae199794
Daniel Endres <danielendres@gmail.com>
2017-07-19 11:37:06 -0700

    update user model, include password, pass validator and tokens

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,8 @@
 // load in mongoose (library)
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
+// load in validator
+const validator = require('validator');
 
 // create the mongoose model for user
 var User = mongoose.model('User', {
@@ -7,8 +10,28 @@ var User = mongoose.model('User', {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email.'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
 
 // export the model
