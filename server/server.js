066f6084804770be2e12067d5c5f04b249d2dce6
Daniel Endres <danielendres@gmail.com>
2017-07-19 11:52:52 -0700

    add POST /users to server.js

--- server/server.js
+++ server/server.js
@@ -113,6 +113,19 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 
+// POST /users ... use pick to pull off email and password, avoid exposing access to tokens
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  var user = new User(body);
+
+  user.save().then((userDoc) => {
+    res.send(userDoc);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
